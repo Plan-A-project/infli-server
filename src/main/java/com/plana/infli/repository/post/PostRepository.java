53bddd4cbdd515c75e503773a5f41390dce296a1
MinjeongKong <kong1301@ajou.ac.kr>
2023-06-06 16:34:15 +0900

    chore: 게시글, 게시글 좋아요 레포지토리 설정 (#13)
    - PostRepository, PostLikeRepository 생성
    
    Signed-off-by: MinjeongKong <kong1301@naver.com>

---
+++ src/main/java/com/plana/infli/repository/post/PostRepository.java
@@ -0,0 +1,7 @@
+package com.plana.infli.repository.post;
+
+import com.plana.infli.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
