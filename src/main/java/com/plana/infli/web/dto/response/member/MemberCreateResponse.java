a6657c0de12ec712b5ae099fd7bc21aa6c339d4d
dukcode <duk9741@gmail.com>
2023-06-20 19:11:03 +0900

    feat: 로그인 API response Role 추가 (#27)
    Signed-off-by: dukcode <duk9741@gmail.com>

--- src/main/java/com/plana/infli/web/dto/response/member/MemberCreateResponse.java
+++ src/main/java/com/plana/infli/web/dto/response/member/MemberCreateResponse.java
@@ -1,6 +1,7 @@
 package com.plana.infli.web.dto.response.member;
 
 import com.plana.infli.domain.Member;
+import com.plana.infli.domain.Role;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,13 +14,15 @@ public class MemberCreateResponse {
 	private String name;
 	private String nickname;
 
-	private MemberCreateResponse(String email, String name, String nickname) {
+	private Role role;
+
+	private MemberCreateResponse(String email, String name, String nickname, Role role) {
 		this.email = email;
 		this.name = name;
 		this.nickname = nickname;
 	}
 
 	public static MemberCreateResponse from(Member member) {
-		return new MemberCreateResponse(member.getEmail(), member.getName(), member.getNickname());
+		return new MemberCreateResponse(member.getEmail(), member.getName(), member.getNickname(), member.getRole());
 	}
 }
