2815d0e00a9a156d1074f0d52ed2e5897d1dd911
dukcode <duk9741@gmail.com>
2023-07-26 05:29:14 +0900

    feat: 기업 회원 가입 및 인증 API 구현 (#123)
    Signed-off-by: dukcode <duk9741@gmail.com>

--- src/main/java/com/plana/infli/domain/EmailAuthentication.java
+++ src/main/java/com/plana/infli/domain/EmailAuthentication.java
@@ -1,8 +1,5 @@
 package com.plana.infli.domain;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -11,6 +8,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,42 +19,52 @@ import lombok.NoArgsConstructor;
 @Entity
 public class EmailAuthentication extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "email_authentication_id")
-	private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "email_authentication_id")
+  private Long id;
 
-	private String email;
+  private String email;
 
-	@Column(nullable = false)
-	private String secret;
+  @Column(nullable = false)
+  private String secret;
 
-	@Column(nullable = false)
-	private LocalDateTime expirationTime;
+  @Column(nullable = false)
+  private LocalDateTime expirationTime;
 
-	private String certificateUrl;
+  private String certificateUrl;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_id")
-	private Member member;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
 
-	public static EmailAuthentication createEmailAuthentication(Member member) {
-		EmailAuthentication emailAuthentication = new EmailAuthentication();
-		emailAuthentication.secret = UUID.randomUUID().toString();
-		emailAuthentication.expirationTime = LocalDateTime.now().plusMinutes(30);
-		emailAuthentication.member = member;
+  public static EmailAuthentication createEmailAuthentication(Member member) {
+    EmailAuthentication emailAuthentication = new EmailAuthentication();
+    emailAuthentication.secret = UUID.randomUUID().toString();
+    emailAuthentication.expirationTime = LocalDateTime.now().plusMinutes(30);
+    emailAuthentication.member = member;
 
-		return emailAuthentication;
-	}
+    return emailAuthentication;
+  }
 
-	public static EmailAuthentication createStudentAuthentication(Member member, String studentEmail) {
-		EmailAuthentication emailAuthentication = new EmailAuthentication();
-		emailAuthentication.secret = UUID.randomUUID().toString();
-		emailAuthentication.expirationTime = LocalDateTime.now().plusMinutes(30);
-		emailAuthentication.member = member;
-		emailAuthentication.email = studentEmail;
+  public static EmailAuthentication createStudentAuthentication(Member member,
+      String studentEmail) {
+    EmailAuthentication emailAuthentication = new EmailAuthentication();
+    emailAuthentication.secret = UUID.randomUUID().toString();
+    emailAuthentication.expirationTime = LocalDateTime.now().plusMinutes(30);
+    emailAuthentication.member = member;
+    emailAuthentication.email = studentEmail;
 
-		return emailAuthentication;
-	}
+    return emailAuthentication;
+  }
+
+  public static EmailAuthentication createCompanyAuthentication(Member member) {
+    EmailAuthentication emailAuthentication = new EmailAuthentication();
+    emailAuthentication.secret = UUID.randomUUID().toString();
+    emailAuthentication.expirationTime = LocalDateTime.now().plusDays(30);
+    emailAuthentication.member = member;
+
+    return emailAuthentication;
+  }
 }
 
--- src/main/java/com/plana/infli/web/controller/MemberController.java
+++ src/main/java/com/plana/infli/web/controller/MemberController.java
@@ -1,5 +1,11 @@
 package com.plana.infli.web.controller;
 
+import com.plana.infli.service.MailService;
+import com.plana.infli.web.dto.request.member.StudentAuthenticationRequest;
+import com.plana.infli.web.resolver.AuthenticatedPrincipal;
+import jakarta.mail.MessagingException;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,42 +13,51 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.plana.infli.service.MailService;
-import com.plana.infli.web.dto.request.member.StudentAuthenticationRequest;
-import com.plana.infli.web.resolver.AuthenticatedPrincipal;
-
-import lombok.RequiredArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RequestMapping("/member")
 @RestController
 public class MemberController {
 
-	private final MailService mailService;
+  private final MailService mailService;
 
-	@PostMapping("/email/auth/send")
-	public ResponseEntity<Void> sendMemberEmailAuthenticationEmail(@AuthenticatedPrincipal String email) {
-		mailService.sendMemberAuthenticationEmail(email);
-		return ResponseEntity.ok().build();
-	}
+  @PostMapping("/email/auth/send")
+  public ResponseEntity<Void> sendMemberEmailAuthenticationEmail(
+      @AuthenticatedPrincipal String email) {
+    mailService.sendMemberAuthenticationEmail(email);
+    return ResponseEntity.ok().build();
+  }
 
-	@PostMapping("/student/auth/send")
-	public ResponseEntity<Void> sendStudentAuthenticationEmail(@AuthenticatedPrincipal String email,
-		@RequestBody StudentAuthenticationRequest request) {
-		mailService.sendStudentAuthenticationEmail(email, request);
-		return ResponseEntity.ok().build();
-	}
+  @PostMapping("/student/auth/send")
+  public ResponseEntity<Void> sendStudentAuthenticationEmail(@AuthenticatedPrincipal String email,
+      @RequestBody StudentAuthenticationRequest request) {
+    mailService.sendStudentAuthenticationEmail(email, request);
+    return ResponseEntity.ok().build();
+  }
 
-	@GetMapping("/email/auth/{secret}")
-	public ResponseEntity<Void> authenticateMemberEmail(@PathVariable String secret) {
-		mailService.authenticateMemberEmail(secret);
-		return ResponseEntity.ok().build();
-	}
+  @PostMapping("/company/auth/send")
+  public ResponseEntity<Void> sendCompanyAuthenticationEmail(@AuthenticatedPrincipal String email,
+      @RequestBody MultipartFile file) throws MessagingException, IOException {
+    mailService.sendCompanyAuthenticationEmail(email, file);
+    return ResponseEntity.ok().build();
+  }
 
-	@GetMapping("/student/auth/{secret}")
-	public ResponseEntity<Void> authenticateStudent(@PathVariable String secret) {
-		mailService.authenticateStudent(secret);
-		return ResponseEntity.ok().build();
-	}
+  @GetMapping("/email/auth/{secret}")
+  public ResponseEntity<Void> authenticateMemberEmail(@PathVariable String secret) {
+    mailService.authenticateMemberEmail(secret);
+    return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/company/auth/{secret}")
+  public ResponseEntity<Void> authenticateCompany(@PathVariable String secret) {
+    mailService.authenticateCompany(secret);
+    return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/student/auth/{secret}")
+  public ResponseEntity<Void> authenticateStudent(@PathVariable String secret) {
+    mailService.authenticateStudent(secret);
+    return ResponseEntity.ok().build();
+  }
 }
