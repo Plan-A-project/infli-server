86f6f84c3273d57c19a3cf9688631418edb84cd3
Lee Youngjin <youngjin8743@gmail.com>
2023-07-25 20:00:09 +0900

    [feat] : 글 좋아요 누르기   (#118)
    -  글 좋아요 누르기 기능 추가
    
    Signed-off-by: jin8743 <youngjin8743@gmail.com>

--- src/main/java/com/plana/infli/repository/postlike/PostLikeRepository.java
+++ src/main/java/com/plana/infli/repository/postlike/PostLikeRepository.java
@@ -1,9 +1,12 @@
 package com.plana.infli.repository.postlike;
 
+import com.plana.infli.domain.Member;
+import com.plana.infli.domain.Post;
 import com.plana.infli.domain.PostLike;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
 
-    PostLike findByPostIdAndMemberId(Long postId, Long memberId);
+    Optional<PostLike> findByPostAndMember(Post post, Member member);
 }
