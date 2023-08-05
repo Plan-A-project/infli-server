d5975546289b471e94355497eb1f6db38c44e4f4
Lee Youngjin <youngjin8743@gmail.com>
2023-08-05 10:20:26 +0900

    feat : 일반글 수정 API 코드 리펙토링  (#164)
    기존 글 수정 API를 2개로 분리하여 일반글 수정 API 생성
    
    Signed-off-by: jin8743 youngjin8743@gmail.com

--- src/main/java/com/plana/infli/web/dto/request/post/edit/normal/EditNormalPostRequest.java
+++ src/main/java/com/plana/infli/web/dto/request/post/edit/normal/EditNormalPostRequest.java
@@ -1,19 +1,15 @@
-package com.plana.infli.web.dto.request.post.edit;
+package com.plana.infli.web.dto.request.post.edit.normal;
 
-
-import com.plana.infli.web.dto.request.post.edit.EditPostServiceRequest.EditRecruitmentServiceRequest;
 import jakarta.annotation.Nullable;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class EditPostRequest {
+public class EditNormalPostRequest {
 
     @NotNull(message = "글 Id 번호를 입력해주세요")
     private Long postId;
@@ -27,64 +23,25 @@ public class EditPostRequest {
     @Nullable
     private String thumbnailUrl;
 
-    @Nullable
-    @Valid
-    private EditRecruitmentRequest recruitment;
-
-
     @Builder
-    public EditPostRequest(Long postId, String title, String content, String thumbnailUrl,
-            EditRecruitmentRequest recruitment) {
+    public EditNormalPostRequest(Long postId, String title,
+            String content, @Nullable String thumbnailUrl) {
 
         this.postId = postId;
         this.title = title;
         this.content = content;
         this.thumbnailUrl = thumbnailUrl;
-        this.recruitment = recruitment;
     }
 
-    public EditPostServiceRequest toServiceRequest(String email) {
-        return EditPostServiceRequest.builder()
+    public EditNormalPostServiceRequest toServiceRequest(String email) {
+        return EditNormalPostServiceRequest.builder()
                 .email(email)
                 .postId(postId)
                 .title(title)
                 .content(content)
                 .thumbnailUrl(thumbnailUrl)
-                .recruitment(recruitment != null ? recruitment.toServiceRequest() : null)
                 .build();
     }
-
-
-    @Getter
-    @NoArgsConstructor
-    public static class EditRecruitmentRequest {
-
-        @NotNull(message = "회사명을 입력해주세요")
-        private String companyName;
-
-        @NotNull(message = "모집 시작일을 입력해주세요")
-        private LocalDateTime startDate;
-
-        @NotNull(message = "모집 종료일을 입력해주세요")
-        private LocalDateTime endDate;
-
-        @Builder
-        private EditRecruitmentRequest(String companyName, LocalDateTime startDate,
-                LocalDateTime endDate) {
-            this.companyName = companyName;
-            this.startDate = startDate;
-            this.endDate = endDate;
-        }
-
-        public EditRecruitmentServiceRequest toServiceRequest() {
-            return EditRecruitmentServiceRequest.builder()
-                    .companyName(companyName)
-                    .startDate(startDate)
-                    .endDate(endDate)
-                    .build();
-        }
-    }
-
 
 
 }
--- src/main/java/com/plana/infli/web/dto/request/post/edit/normal/EditNormalPostServiceRequest.java
+++ src/main/java/com/plana/infli/web/dto/request/post/edit/normal/EditNormalPostServiceRequest.java
@@ -1,12 +1,11 @@
 package com.plana.infli.web.dto.request.post.edit.normal;
 
 import jakarta.annotation.Nullable;
-import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class EditPostServiceRequest {
+public class EditNormalPostServiceRequest {
 
     private final String email;
 
@@ -19,37 +18,14 @@ public class EditPostServiceRequest {
     @Nullable
     private final String thumbnailUrl;
 
-    @Nullable
-    private final EditRecruitmentServiceRequest recruitment;
-
-
     @Builder
-    public EditPostServiceRequest(String email, Long postId, String title, String content,
-            String thumbnailUrl, EditRecruitmentServiceRequest recruitment) {
+    public EditNormalPostServiceRequest(String email, Long postId,
+            String title, String content, @Nullable String thumbnailUrl) {
         this.email = email;
         this.postId = postId;
         this.title = title;
         this.content = content;
         this.thumbnailUrl = thumbnailUrl;
-        this.recruitment = recruitment;
-    }
-
-    @Getter
-    public static class EditRecruitmentServiceRequest {
-
-        private final String companyName;
-
-        private final LocalDateTime startDate;
-
-        private final LocalDateTime endDate;
-
-        @Builder
-        public EditRecruitmentServiceRequest(String companyName, LocalDateTime startDate,
-                LocalDateTime endDate) {
-            this.companyName = companyName;
-            this.startDate = startDate;
-            this.endDate = endDate;
-        }
     }
 
 }
