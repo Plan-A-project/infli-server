042fca0eec15cf1e8540b9c0f598e6fbf98c9adc
Lee Youngjin <youngjin8743@gmail.com>
2023-08-12 21:27:31 +0900

    bug : 업로드한 원본 사진을 썸네일로 전환하는 기능 안되는 문제 해결(#191)
    원본 사진을 썸네일 사진으로 화질 열화하는 기능이 안되는 문제 해결
    
    Signed-off-by: jin8743 youngjin8743@gmail.com

--- src/main/java/com/plana/infli/service/aop/upload/UploadAspect.java
+++ src/main/java/com/plana/infli/service/aop/upload/UploadAspect.java
@@ -1,40 +1,30 @@
 package com.plana.infli.service.aop.upload;
 
 import static com.plana.infli.exception.custom.InternalServerErrorException.IMAGE_UPLOAD_FAILED;
-import static com.plana.infli.exception.custom.InternalServerErrorException.POST_VIEW_FAILED;
 
 import com.plana.infli.exception.custom.InternalServerErrorException;
-import com.plana.infli.service.aop.retry.Retry;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @Slf4j
+@Order(Ordered.LOWEST_PRECEDENCE - 1)
 public class UploadAspect {
 
 
     @Around("@annotation(upload)")
-    public Object doRetry(ProceedingJoinPoint joinPoint, Upload upload) throws Throwable {
-
-        int maxRetry = upload.value();
-
-        for (int retryCount = 1; retryCount <= maxRetry; retryCount++) {
-            try {
-                return joinPoint.proceed();
-            } catch (IOException e) {
-                try {
-                    log.info("[retry] try count={}/{}", retryCount, maxRetry);
-                    Thread.sleep(50);
-                } catch (InterruptedException ignored) {
-                }
-            }
+    public Object upload(ProceedingJoinPoint joinPoint, Upload upload) throws Throwable {
+        try {
+            return joinPoint.proceed();
+        } catch (IOException e) {
+            throw new InternalServerErrorException(IMAGE_UPLOAD_FAILED);
         }
-        throw new InternalServerErrorException(IMAGE_UPLOAD_FAILED);
     }
 }
