83594861d78587018436d59b8e88b82cd61e4bc9
Lee Youngjin <youngjin8743@gmail.com>
2023-08-27 22:45:11 +0900

    bug : 특정 글에 작성된 댓글 목록 조회시 500 에러 발생하는 문제 해결 (279)
    Signed-off-by: jin8743 youngjin8743@gmail.com

--- src/main/java/com/plana/infli/web/controller/BoardController.java
+++ src/main/java/com/plana/infli/web/controller/BoardController.java
@@ -6,20 +6,15 @@ import com.plana.infli.web.dto.request.board.popular.edit.EditPopularBoardSequen
 import com.plana.infli.web.dto.response.board.settings.board.BoardListResponse;
 import com.plana.infli.web.dto.response.board.settings.polularboard.PopularBoardsSettingsResponse;
 import com.plana.infli.web.dto.response.board.view.PopularBoardsResponse;
+import com.plana.infli.web.resolver.AuthenticatedPrincipal;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,20 +26,20 @@ public class BoardController {
 
     @GetMapping("/boards/popular/exists")
     @Operation(summary = "인기 게시판 기본 설정값 존재하는지 확인")
-    public boolean isPopularBoardCreated(@AuthenticationPrincipal String username) {
+    public boolean isPopularBoardCreated(@AuthenticatedPrincipal String username) {
         return boardService.popularBoardExistsBy(username);
     }
 
 
     @GetMapping("/boards/popular")
     @Operation(summary = "해당 사용자가 보고싶다고 설정한 인기 게시판 목록 조회")
-    public PopularBoardsResponse loadEnabledPopularBoards(@AuthenticationPrincipal String username) {
+    public PopularBoardsResponse loadEnabledPopularBoards(@AuthenticatedPrincipal String username) {
         return boardService.loadEnabledPopularBoardsBy(username);
     }
 
     @PostMapping("/boards/popular")
     @Operation(summary = "인기 게시판 기본 설정값 생성")
-    public void createDefaultPopularBoards(@AuthenticationPrincipal String username) {
+    public void createDefaultPopularBoards(@AuthenticatedPrincipal String username) {
         boardService.createDefaultPopularBoards(username);
     }
 
@@ -56,13 +51,13 @@ public class BoardController {
     @GetMapping("/settings/boards/popular")
     @Operation(description = "인기 게시판을 회원이 보고 싶은 순서대로 변경하기 위해, 인기 게시판 목록 조회")
     public PopularBoardsSettingsResponse loadEnabledPopularBoardsForSetting(
-            @AuthenticationPrincipal String username) {
+            @AuthenticatedPrincipal String username) {
         return boardService.loadEnabledPopularBoardsForSettingBy(username);
     }
 
     @PatchMapping("/settings/boards/popular")
     @Operation(description = "인기 게시판을 회원이 보고 싶은 순서대로 변경")
-    public void changePopularBoardSequence(@AuthenticationPrincipal String username,
+    public void changePopularBoardSequence(@AuthenticatedPrincipal String username,
             @RequestBody @Validated EditPopularBoardSequenceRequest request) {
         boardService.changePopularBoardSequence(request.toServiceRequest(username));
     }
@@ -70,14 +65,14 @@ public class BoardController {
 
     @GetMapping("/settings/boards")
     @Operation(description = "해당 대학에 존재하는 모든 게시판 조회")
-    public BoardListResponse listAllBoards(@AuthenticationPrincipal String username) {
+    public BoardListResponse listAllBoards(@AuthenticatedPrincipal String username) {
 
         return boardService.loadAllBoard(username);
     }
 
     @PostMapping("/settings/boards/popular")
     @Operation(description = "모든 인기 게시판중 보고싶은 인기 게시판만 조회되도록 선택")
-    public void changeBoardVisibility(@AuthenticationPrincipal String username,
+    public void changeBoardVisibility(@AuthenticatedPrincipal String username,
             @RequestBody @Validated ChangePopularBoardVisibilityRequest request) {
 
         boardService.changeBoardVisibility(request.toServiceRequest(username));
