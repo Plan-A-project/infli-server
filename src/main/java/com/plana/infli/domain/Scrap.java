dc8aca67b2f276aebdbadac16b16176ed6bb67cb
Lee Youngjin <youngjin8743@gmail.com>
2023-10-11 21:09:57 +0900

    스크랩 Entity 컬럼 추가

--- src/main/java/com/plana/infli/domain/Scrap.java
+++ src/main/java/com/plana/infli/domain/Scrap.java
@@ -1,5 +1,6 @@
 package com.plana.infli.domain;
 
+import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.*;
 import static lombok.AccessLevel.*;
 
@@ -7,6 +8,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,4 +22,12 @@ public class Scrap {
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "scrap_id")
     private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 }
