d3d0b52ba5453b8b7e416b1018f33a7a99a0ab87
Lee Youngjin <youngjin8743@gmail.com>
2023-10-14 21:45:39 +0900

    feat : 좋아하는글 스크랩 하는 기능 추가  (394)
    Signed-off-by: jin8743 youngjin8743@gmail.com

--- src/main/java/com/plana/infli/repository/scrap/ScrapRepository.java
+++ src/main/java/com/plana/infli/repository/scrap/ScrapRepository.java
@@ -1,8 +1,12 @@
 package com.plana.infli.repository.scrap;
 
+import com.plana.infli.domain.Member;
+import com.plana.infli.domain.Post;
 import com.plana.infli.domain.Scrap;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScrapRepository extends JpaRepository<Scrap, Long>, ScrapRepositoryCustom {
 
+
+    public boolean existsByPostAndMember(Post post, Member member);
 }
--- src/main/java/com/plana/infli/service/ScrapService.java
+++ src/main/java/com/plana/infli/service/ScrapService.java
@@ -1,5 +1,17 @@
 package com.plana.infli.service;
 
+import static com.plana.infli.domain.Scrap.*;
+import static com.plana.infli.infra.exception.custom.ConflictException.SCRAP_ALREADY_EXISTS;
+import static com.plana.infli.infra.exception.custom.NotFoundException.*;
+
+import com.plana.infli.domain.Member;
+import com.plana.infli.domain.Post;
+import com.plana.infli.domain.Scrap;
+import com.plana.infli.infra.exception.custom.ConflictException;
+import com.plana.infli.infra.exception.custom.NotFoundException;
+import com.plana.infli.repository.member.MemberRepository;
+import com.plana.infli.repository.post.PostRepository;
+import com.plana.infli.repository.scrap.ScrapRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -9,5 +21,40 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ScrapService {
 
+    private final MemberRepository memberRepository;
 
+    private final ScrapRepository scrapRepository;
+
+    private final PostRepository postRepository;
+
+    public void createScrap(String username, Long postId) {
+
+        Member member = findMemberBy(username);
+
+        Post post = findPostBy(postId);
+
+        checkScrapDuplicate(post, member);
+
+        Scrap scrap = create(post, member);
+
+        scrapRepository.save(scrap);
+    }
+
+    private void checkScrapDuplicate(Post post, Member member) {
+        if (scrapRepository.existsByPostAndMember(post, member)) {
+            throw new ConflictException(SCRAP_ALREADY_EXISTS);
+        }
+    }
+
+
+    private Member findMemberBy(String username) {
+        return memberRepository.findActiveMemberBy(username)
+                .orElseThrow(() -> new NotFoundException(MEMBER_NOT_FOUND));
+    }
+
+
+    private Post findPostBy(Long postId) {
+        return postRepository.findPostById(postId)
+                .orElseThrow(() -> new NotFoundException(POST_NOT_FOUND));
+    }
 }
