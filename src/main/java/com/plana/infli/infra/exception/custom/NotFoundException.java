67d197f928aab571396a19d47434ac972e3523b3
Lee Youngjin <youngjin8743@gmail.com>
2023-11-01 21:34:19 +0900

    Revert "chore : 스크랩 취소 Controller 레이어 추가 컬럼 추가 (#396)"
    This reverts commit 752647994131046ed03065240d3572402fdb0f4e.

--- src/main/java/com/plana/infli/infra/exception/custom/NotFoundException.java
+++ src/main/java/com/plana/infli/infra/exception/custom/NotFoundException.java
@@ -12,9 +12,7 @@ public class NotFoundException extends DefaultException {
 
     public static final String POST_NOT_FOUND = "게시글이 존재하지 않거나 삭제되었습니다";
 
-	public static final String COMMENT_NOT_FOUND = "댓글이 존재하지 않거나 삭제되었습니다";
-
-	public static final String SCRAP_NOT_FOUND = "스크랩이 존재하지 않습니다";
+    public static final String COMMENT_NOT_FOUND = "댓글이 존재하지 않거나 삭제되었습니다";
 
 
 	public NotFoundException(String message) {
--- src/main/java/com/plana/infli/repository/scrap/ScrapRepository.java
+++ src/main/java/com/plana/infli/repository/scrap/ScrapRepository.java
@@ -3,13 +3,10 @@ package com.plana.infli.repository.scrap;
 import com.plana.infli.domain.Member;
 import com.plana.infli.domain.Post;
 import com.plana.infli.domain.Scrap;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ScrapRepository extends JpaRepository<Scrap, Long>, ScrapRepositoryCustom {
 
 
-    boolean existsByPostAndMember(Post post, Member member);
-
-    Optional<Scrap> findByPostAndMember(Post post, Member member);
+    public boolean existsByPostAndMember(Post post, Member member);
 }
